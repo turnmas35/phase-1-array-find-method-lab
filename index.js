17ff67502cd5d1787f03b5f4e8ff8ac893a9a463
Matthew Turner <turnmas35@gmail.com>
2023-10-21 14:38:02 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code your solution here
+function superbowlWin(records) {
+    const winningRecord = records.find(record => record.result === "W");
+    return winningRecord ? winningRecord.year : undefined;
+  }
